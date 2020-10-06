a1ca98b6743bae8ef47e9e2e668fc401de49eb4e
Martin Šimon <mart.simon17@gmail.com>
2020-10-06 13:13:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function Cum() {
+  let jm = document.getElementById("jmeno").value;
+  console.log(jm);
+  //alert("Zdar " + jm + "e :0");
+}
+
+
+
+function count() {
+  let num1 = document.getElementById("num1").valueAsNumber;
+  let num2 = document.getElementById("num2").valueAsNumber;
+  let num = num1 + num2; 
+
+  alert(num);
+}
